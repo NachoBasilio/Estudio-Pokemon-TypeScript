823f2580587b3b382994a545484d62ced9f9d0a2
IgnaDev <arcortoon@gmail.com>
2023-09-23 20:08:02 -0300

    Terminado fetch

---
+++ src/helpers/fetch.ts
@@ -0,0 +1,39 @@
+
+interface Pokemon {
+    name: string;
+    img: string;
+    type: string;
+    number: string;
+}
+
+const fetchPokemon = async (): Promise<Pokemon[]>  => {
+    const pokemons: Pokemon[] = [];
+
+    const apiUrl = 'https://pokeapi.co/api/v2/pokemon';
+    
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    const { results } = data;
+
+    for (const pokemon of results) {
+        const response = await fetch(pokemon.url);
+        const data = await response.json();
+
+        const { id, name, sprites, types } = data;
+
+        const pokemonFormatted = {
+            name,
+            img: sprites.front_default,
+            type: types[0].type.name,
+            number: `#${id.toString().padStart(3, '0')}`
+        }
+
+        pokemons.push(pokemonFormatted);
+    }
+
+    return pokemons;
+}
+
+
+export default fetchPokemon;
